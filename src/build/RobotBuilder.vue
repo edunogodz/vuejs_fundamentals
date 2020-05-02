c281440491d1d6f040675d1f5af66a3920f30e11
carlos <edunogodz@gmail.com>
2020-05-02 17:37:12 -0300

    Routing from Page to Page

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -80,10 +80,21 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals:0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -118,6 +129,7 @@ export default {
       // this.cart.push({ ...robot, cost });
       // eslint-disable-next-line prefer-object-spread
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     },
   },
 };
